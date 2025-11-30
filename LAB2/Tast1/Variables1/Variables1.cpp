f626ada41d5a9ea99352334d6f384770c4639f22
6711800020-Sudo <6711800020@mut.ac.th>
2025-11-30 10:44:19 +0700

    อัพเดต Code การทดลองที่ 2

---
+++ LAB2/Tast1/Variables1/Variables1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int age = 22;
+
+	float height = 174.5;
+
+	string name = "Phonlawat injun";
+
+	cout << "Student Profile" << endl;
+
+	cout << "-----------------------" << endl;
+
+	cout << "Name: " << name << endl;
+
+	cout << "Age : " << age << endl;
+
+	cout << "Height: " << height << " cm" << endl;
+
+	return 0;
+
+}
---
+++ LAB2/Tast1/VariablesTest1/VariablesTest1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int age = 22;
+
+	float GPA = 2.55;
+
+	string name = "Phonlawat injun";
+
+	string StudentID = "6711800020";
+
+	cout << "===========Student Info=========" << endl;
+
+	cout << "Name       : " << name << endl;
+
+	cout << "Age        : " << age << endl;
+
+	cout << "Student ID : " << StudentID << endl;
+
+	cout << "GPA        : " << GPA << endl;
+
+	cout << "================================" << endl;
+
+	return 0;
+
+}
